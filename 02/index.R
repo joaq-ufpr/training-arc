dd4eb7321eb9172272b151b25fc53273c5d80cfb
joaq-ufpr <joaquim.almeida@ufpr.br>
2025-06-15 19:59:13 -0300

    exercicios feito em dplyr

--- 02/index.R
+++ 02/index.R
@@ -1,4 +1,4 @@
-library(tidyverse)
+## library(tidyverse)
 require(dplyr)
 require(magrittr)
 library(data.table)
@@ -130,13 +130,160 @@ car_crash %>%
 
 ### Exercicios
 ## Utilizando o banco de dados car_crash, faça o que se pede:
+
 ## Selecione as variáveis data, tipo_de_ocorrencia, automovel, bicicleta, onibus, caminhao, moto, trator, outros e total.
+View(head(car_crash))
+
+ex1 <- car_crash %>%
+    select(data, tipo_de_ocorrencia, automovel,
+           bicicleta, onibus, caminhao,
+           moto, trator_maquinas, outros) %>%
+    glimpse()
+
 ## Selecione todas as variáveis que contenham a palavra feridos.
+
+ex2 <- car_crash %>%
+    select(contains("feridos")) %>%
+    glimpse()
+
 ## Selecione todas as variáveis numéricas.
+
+ex3 <- car_crash %>%
+    select(where(is.numeric)) %>%
+    glimpse()
+
 ## Selecione todas as variáveis lógicas.
+
+ex4 <- car_crash %>%
+    select(where(is.logical)) %>%
+    glimpse()
+
 ## Selecione todas as variáveis que terminem com a letra o.
+
+ex4 <- car_crash %>%
+    select(ends_with("o")) %>%
+    glimpse()
+
 ## Selecione todas as variáveis que iniciem com a letra t.
+
+ex5 <- car_crash %>%
+    select(starts_with("t")) %>%
+    glimpse()
+
 ## Filtre as observações com pelo menos 5 carros E 3 motos envolvidos no acidente.
+
+ex6 <- car_crash %>%
+    filter(automovel >= 5 & moto >= 3) %>%
+    glimpse()
+
 ## Filtre as observações com pelo menos 5 carros OU 3 motos envolvidos no acidente.
+
+ex7 <- car_crash %>%
+    filter(automovel >= 5 | moto >= 3) %>%
+    glimpse()
+
 ## Filtre as observações com vítimas.
-## Filtre as observações com pelo menos 5 carros OU 3 motos envolvidos no acidente E que ocorreram em alguma das seguintes operadoras: “Autopista Regis Bittencourt”, “Autopista Litoral Sul”, “Via Sul”.
+
+ex8 <- car_crash %>%
+    filter(levemente_feridos > 0 &
+           moderadamente_feridos > 0 &
+           mortos > 0) %>%
+    glimpse()
+
+## Filtre as observações com pelo menos 5 carros OU 3 motos envolvidos
+## no acidente E que ocorreram em alguma das seguintes
+## operadoras: “Autopista Regis Bittencourt”,
+## “Autopista Litoral Sul”, “Via Sul”.
+
+autopistas <- c("Autopista Regis Bittencourt",
+                "Autopista Litoral Sul", "Via Sul")
+ex8 <- car_crash %>%
+    filter((automovel >= 5 | moto >= 3) &
+           (lugar_acidente %in% autopistas)) %>%
+    glimpse()
+
+### Agrupamento de dados
+# podemos usar a função summarise() e group_by()
+
+# summarise() é super utíl para calcular estatísticas
+# resumidas para uma coluna ou um conjunto de colunas
+
+# tabela descritiva para a var "levemente_feridos"
+tabela <- car_crash %>%
+    summarise(n = n(),
+              freq_rel = n() / nrow(car_crash),
+              freq_per = (n() / nrow(car_crash)) * 100,
+              media = mean(levemente_feridos, na.rm = TRUE),
+              Q1 = quantile(levemente_feridos, 0.25, type = 5, na.rm = TRUE),
+              Q2 = quantile(levemente_feridos, 0.5, type = 5, na.rm = TRUE),
+              Q3 = quantile(levemente_feridos, 0.75, type = 5, na.rm = TRUE),
+              var = var(levemente_feridos, na.rm = TRUE),
+              sd = sd(levemente_feridos, na.rm = TRUE),
+              min = min(levemente_feridos, na.rm = TRUE),
+              max = max(levemente_feridos, na.rm = TRUE))
+
+# uso do group_by()
+# A função group_by() é usada para agrupar o conjunto de dados por uma ou mais colunas.
+# Isso cria um contexto em que a função summarise() pode calcular estatísticas específicas para cada grupo.
+
+# Estamos interessados em uma tabela descritiva para a variável levemente_feridos por tipo_de_ocorrencia.
+tabela <- car_crash %>%
+    filter(tipo_de_ocorrencia %in% c("sem vítima", "com vítima")) %>%
+    group_by(tipo_de_ocorrencia) %>%
+    summarise(n = n(),
+              freq_rel = n() / nrow(car_crash),
+              freq_per = (n() / nrow(car_crash)) * 100,
+              media = mean(levemente_feridos, na.rm = TRUE),
+              Q1 = quantile(levemente_feridos, 0.25, type = 5, na.rm = TRUE),
+              Q2 = quantile(levemente_feridos, 0.5, type = 5, na.rm = TRUE),
+              Q3 = quantile(levemente_feridos, 0.75, type = 5, na.rm = TRUE),
+              var = var(levemente_feridos, na.rm = TRUE),
+              sd = sd(levemente_feridos, na.rm = TRUE),
+              min = min(levemente_feridos, na.rm = TRUE),
+              max = max(levemente_feridos, na.rm = TRUE))
+tabela
+
+### Exercicios
+
+## Utilizando o banco de dados starwars faça o que se pede:
+
+## Qual é o número total de espécies únicas presentes? Qual a frequência de indivíduos por espécie?
+
+num_especies <- starwars %>%
+    distinct(species) %>%
+    select(species) %>%
+    ## na.omit() %>%
+    summarise(n_especies = n())
+num_especies
+
+tabela1 <- starwars %>%
+    ## filter(!is.na(species)) %>%
+    group_by(species) %>%
+    summarise(frequencia_relativa = (n() / nrow(starwars)))
+
+View(tabela1)
+## Calcule a altura média de personagens masculinos e femininos.
+
+unique(starwars$gender)
+
+tabela_altura_media_genero <- starwars %>%
+    ## filter(!is.na(gender)) %>%
+    group_by(gender) %>%
+    summarise(frequencia_relativa = (n() / nrow(starwars)))
+
+View(tabela_altura_media)
+
+## Qual é a média de idade dos personagens de cada espécie para personagens masculinos?
+
+tab_idade_media_por_especie_masc <- starwars  %>%
+    filter(gender == "masculine") %>%
+    group_by(species) %>%
+    summarise(frequencia_relativa = (n() / nrow(starwars)),
+              media_idade = mean(starwars$birth_year, na.rm = TRUE))
+View(tab_idade_media_por_especie_masc)
+
+
+## Para cada espécie presente na base de dados, identifique o personagem mais velho e sua idade correspondente.
+# não tem como fazer isso.
+
+require(lubridate)
